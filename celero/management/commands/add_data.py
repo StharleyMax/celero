9f37038fae0e6856a314adc5d74c42335a342920
Stharley Maxwell <stharleymax@gmail.com>
2022-03-30 14:05:12 -0300

    feat: dataset save in database

--- celero/management/commands/add_data.py
+++ celero/management/commands/add_data.py
@@ -1,7 +1,7 @@
 from dataclasses import replace
-from   django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand
 import pandas as pd
-from celero.models import Athlete
+from celero.models import Athlete, Event, AthleteHasEvents, EventsHasGame, Game
 from sqlalchemy import create_engine
 
 class Command(BaseCommand):
@@ -10,7 +10,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         file = 'athlete_events.csv'
         df = pd.read_csv(file)
-        print(df)
 
         engine = create_engine('sqlite:///db.sqlite3' )
-        df.to_sql(Athlete._meta.db_table, if_exists='replace', con=engine, index=False)
+
+
+        Athlete.create(df, engine)
+        Event.create(df, engine)
+        AthleteHasEvents.create(df, engine)
+        Game.create(df, engine)
+        EventsHasGame.create(df, engine)
