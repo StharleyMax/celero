767e823a0eb6873f3b92c5b599147eddbec45b8f
Stharley Maxwell <stharleymax@gmail.com>
2022-03-30 09:55:37 -0300

    chore: add sqlalchemy

--- celero/management/commands/add_data.py
+++ celero/management/commands/add_data.py
@@ -1,5 +1,8 @@
+from dataclasses import replace
 from   django.core.management.base import BaseCommand
 import pandas as pd
+from celero.models import Athlete
+from sqlalchemy import create_engine
 
 class Command(BaseCommand):
     help = "A command to add dataset to databases"
@@ -8,3 +11,6 @@ class Command(BaseCommand):
         file = 'athlete_events.csv'
         df = pd.read_csv(file)
         print(df)
+
+        engine = create_engine('sqlite:///db.sqlite3' )
+        df.to_sql(Athlete._meta.db_table, if_exists='replace', con=engine, index=False)
